17210d9a6f17d678141b31a46a06aa1b7c331963
SoutionSathee <139756555+SoutionSathee@users.noreply.github.com>
2023-09-18 15:26:26 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,7 +7,7 @@ import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import LoginNavbar from "../components/LoginNavbar";
-import imgFive from "../../public/Images/saathLogo.jpeg";
+
 
 const schema = yup
   .object()
